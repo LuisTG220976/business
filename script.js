756c9933201cb8a868a24eb904198de62d95d322
LuisTG220976 <ltorresggg@hotmail.com>
2022-06-25 22:58:18 -0500

    vv

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("icon_menu").addEventListener("click",mostrar_menu);
+/*document.getElementById("icon_menu").addEventListener("click",mostrar_menu);
 
 function mostrar_menu(){
     document.querySelector(".menu").classList.toggle("mostrar_menu");
@@ -7,10 +7,10 @@ function mostrar_menu(){
 
 
 
-window.onscroll = function(){
+/*window.onscroll = function(){
     var posicion = window.pageXOffset || document.documentElement.scrollTop;
     var elemento1 = document.getElementById("icon_heart");
     var elemento2 = document.getElementById("icon_fire");
     elemento1.style.bottom = posicion * 0.1 + "px";
     elemento1.style.top = posicion * 0.1 + "px";
-}
+}*/
